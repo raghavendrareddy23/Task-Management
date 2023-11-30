d2a8fb963d385cb56b98a5815bd9dff4c74e879a
raghavendrareddy23 <raghavendrareddy.munagala23@gmail.com>
2023-11-30 21:34:13 +0530

    commit

---
+++ backend/models/User.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    trim: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+  },
+  role: {
+    type: String,
+    enum: ['Admin', 'Team Member'],
+    default: 'Team Member'
+  },
+  joiningTime: {
+    type: Date,
+    default: Date.now
+  }
+}, {
+  timestamps: true
+});
+
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
